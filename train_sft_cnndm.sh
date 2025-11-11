a4dcc83c06b97d8606a1cc57fe84371b7220dfc4
Lirong Gao <35567571+lrgao@users.noreply.github.com>
2025-11-11 13:12:42 +0800

    Update train_sft_cnndm.sh

--- train_sft_cnndm.sh
+++ train_sft_cnndm.sh
@@ -1,19 +1,16 @@
-#!/bin/bash
-source ~/.bashrc
-conda activate base
-
-llm_name="qwen2-7b"  # ("qwen2-7b" "llama3-8b" "glm4-9b")
+llm_name="Qwen2-7B-Instruct"  # ("Qwen2-7B-Instruct" "Meta-Llama-3-8B-Instruct" "THUDM-glm-4-9b-chat")
+dataset="cnndm"
 # Stage 1: SFT
 CUDA_VISIBLE_DEVICES=0,1,2,3 python cli_gt.py \
         --do_train \
         --model_name t5 \
-        --output_dir out/SFT-cnndm-${llm_name} \
-        --train_file dataset/cnndm/${llm_name}/train_sft.json \
-        --predict_file dataset/cnndm/${llm_name}/dev_sft.json \
-        --icl_file ./dataset/cnndm/${llm_name}/icl.json \
+        --output_dir out/SFT-${dataset}-${llm_name} \
+        --train_file dataset/w2s/${llm_name}/${dataset}/traindata/sft.json \
+        --predict_file dataset/w2s/${llm_name}/${dataset}/devdata/sft.json \
+        --icl_file dataset/raw/${dataset}/icl.json \
         --model_path t5-base \
         --tokenizer_path t5-base \
-        --dataset cnndm \
+        --dataset ${dataset} \
         --train_batch_size 4 \
         --predict_batch_size 2 \
         --gradient_accumulation_steps 8 \
